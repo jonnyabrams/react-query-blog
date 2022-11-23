4d4f628d9828ad5a57da2d6ca25964c23881a17a
jonnyabrams <jonnyabrams@hotmail.com>
2022-11-23 14:40:03 +0000

    feat: pagination

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -5,18 +5,22 @@ import PostDetail from "./PostDetail";
 
 const maxPostPage = 10;
 
-const fetchPosts = async () => {
+const fetchPosts = async (pageNum) => {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
   return response.json();
 };
 
 const Posts = () => {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  const { data, isLoading, isError, error } = useQuery(["posts"], fetchPosts, {staleTime: 2000});
+  const { data, isLoading, isError, error } = useQuery(
+    ["posts", currentPage],
+    () => fetchPosts(currentPage),
+    { staleTime: 2000 }
+  );
   if (isLoading) return <h3>Loading...</h3>;
   if (isError)
     return (
@@ -40,11 +44,14 @@ const Posts = () => {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => setCurrentPage((prev) => prev - 1)}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button disabled={ currentPage >= maxPostPage } onClick={() => setCurrentPage((prev) => prev + 1)}>
           Next page
         </button>
       </div>
